d685f959a3d2d8edf9bb916128b55cd99817df5f
calvinkcc <calvinkcc0@gmail.com>
2018-07-07 22:01:47 -0400

    5th commit

--- my_company/blog/models.py
+++ my_company/blog/models.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.utils import timezone
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 class Blog(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    slug=models.SlugField(allow_unicode=True,unique=True)
     subject = models.CharField(max_length=300)
     description = models.TextField()
     text = models.TextField()
@@ -16,5 +18,9 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
 
+    def save(self,*args,**kwargs):
+        self.slug = slugify(self.title)
+        super(Blog,self).save(*args,**kwargs)
+
     def get_absolute_url(self):
-        return reverse('blog:blog_detail',args=[str(self.id)])
+        return reverse('blog:blog_detail',kwargs={'slug':self.slug, 'id':self.id})
--- my_company/blog/urls.py
+++ my_company/blog/urls.py
@@ -9,6 +9,6 @@ app_name = 'blog'
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Blog.objects.all().order_by("-date")[:10], template_name="blog/blog.html"),name='blog_list'),
-    url(r'^(?P<pk>\d+)$', views.PostDetailView.as_view(), name='blog_detail'),
+    url(r'^(?P<slug>[-\w]+)$', views.PostDetailView.as_view(), name='blog_detail'),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
